961145a87787221fce014a09e540cd0546b61c97
Squiro <alesio_sinopoli@hotmail.com>
2021-04-30 12:38:47 -0300

    ADD: exe file

--- exe/run.bat
+++
@@ -1,2 +0,0 @@
-compilador.exe
-pause
--- src/main/java/com/unlam/compilador/Main.java
+++ src/main/java/com/unlam/compilador/Main.java
@@ -1,24 +1,31 @@
+package com.unlam.compilador;
+
 import Analizadores.AnalizadorLexico;
 import Analizadores.AnalizadorSintactico;
 import java_cup.runtime.Symbol;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String args[])
+    public static void main(String args[]) throws IOException
     {
+    	System.out.println(System.getProperty("java.class.path")); 
         String fileName = "prueba.txt";
         try {     	
             @SuppressWarnings("deprecation") AnalizadorSintactico sintactico = new AnalizadorSintactico(new AnalizadorLexico(new FileReader(fileName)));
             sintactico.parse();
+            System.in.read();
         } catch (FileNotFoundException e) {
             System.err.	println("El archivo " + fileName + " no existe");
             e.printStackTrace();
+            System.in.read();
         } catch (Exception e) {
             System.err.println("Hubo un error al ejecutar el programa: ");
             System.err.println(e);
             e.printStackTrace();
+            System.in.read();
         }
     }
 }
