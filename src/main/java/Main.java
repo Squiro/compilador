f3bc71a7f8d1f6a9db6fbdd650ff0281f6a6d040
Squiro <alesio_sinopoli@hotmail.com>
2021-04-30 10:22:05 -0300

    ADD: removed commented code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,16 +9,9 @@ public class Main {
     public static void main(String args[])
     {
         String fileName = "prueba.txt";
-        try {
-        	// AnalizadorLexico lexer = new AnalizadorLexico(new FileReader(fileName));
-        	// Symbol token = null;        	
-        	// do {
-        	// 	token = lexer.next_token();
-        	// }
-        	// while (token != null);       	
+        try {     	
             @SuppressWarnings("deprecation") AnalizadorSintactico sintactico = new AnalizadorSintactico(new AnalizadorLexico(new FileReader(fileName)));
             sintactico.parse();
-            // System.out.println("Se ejecuto el archivo " + fileName);
         } catch (FileNotFoundException e) {
             System.err.	println("El archivo " + fileName + " no existe");
             e.printStackTrace();
